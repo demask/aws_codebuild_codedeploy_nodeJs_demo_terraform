f7821c3396dde7a5a07177d150ab35dd7c38ec66
mdemic <markodemic@gmail.com>
2022-02-03 14:56:10 +0100

    Add interpolation

--- dev/main.tf
+++ dev/main.tf
@@ -1,16 +1,16 @@
 module "task_definition" {
   source = "../modules/task-definition"
 
-  provider_region = var.provider_region
-  aws_account_id = var.aws_account_id
-  task_definition_role = var.task_definition_role
-  task_definition_name = var.task_definition_name
-  requires_compatibilities = var.requires_compatibilities
-  cpu = var.cpu
-  memory = var.memory
-  container_cpu = var.container_cpu
-  port_mappings = var.port_mappings
-  environment_variables = var.environment_variables
-  docker_image_tag = var.docker_image_tag
-  docker_image_name = var.docker_image_name
+  provider_region = "${var.provider_region}"
+  aws_account_id = "${var.aws_account_id}"
+  task_definition_role = "${var.task_definition_role}"
+  task_definition_name = "${var.task_definition_name}"
+  requires_compatibilities = "${var.requires_compatibilities}"
+  cpu = "${var.cpu}"
+  memory = "${var.memory}"
+  container_cpu = "${var.container_cpu}"
+  port_mappings = "${var.port_mappings}"
+  environment_variables = "${var.environment_variables}"
+  docker_image_tag = "${var.docker_image_tag}"
+  docker_image_name = "${var.docker_image_name}"
 }
