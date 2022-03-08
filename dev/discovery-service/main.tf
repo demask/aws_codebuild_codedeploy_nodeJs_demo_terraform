1378e0066782f4be28b29baa4a95b1c606651a27
mdemic <markodemic@gmail.com>
2022-03-08 22:06:43 +0100

    Add job2 and job3 discovery service

--- dev/discovery-service/main.tf
+++ dev/discovery-service/main.tf
@@ -25,3 +25,41 @@ resource "aws_service_discovery_service" "job1_discovery_service" {
     failure_threshold = 1
   }
 }
+
+resource "aws_service_discovery_service" "job2_discovery_service" {
+  name = "job2"
+
+  dns_config {
+    namespace_id = data.terraform_remote_state.service_discovery_namespace.outputs.discovery_namespace_id
+
+    dns_records {
+      ttl  = 100
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+resource "aws_service_discovery_service" "job3_discovery_service" {
+  name = "job3"
+
+  dns_config {
+    namespace_id = data.terraform_remote_state.service_discovery_namespace.outputs.discovery_namespace_id
+
+    dns_records {
+      ttl  = 100
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
--- dev/discovery-service/output.tf
+++ dev/discovery-service/output.tf
@@ -1,3 +1,11 @@
 output "job1_service_discovery_service_arn" {
   value = aws_service_discovery_service.job1_discovery_service.arn
 }
+
+output "job2_service_discovery_service_arn" {
+  value = aws_service_discovery_service.job2_discovery_service.arn
+}
+
+output "job3_service_discovery_service_arn" {
+  value = aws_service_discovery_service.job3_discovery_service.arn
+}
